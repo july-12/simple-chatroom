8c1a832de64d3a0ca22b801179111b398e9a9021
Faith <seventanyb@gmail.com>
2020-11-12 10:54:34 +0800

    remove useless file

---
+++ server/store/index.js
@@ -0,0 +1,81 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (Math.random() * 16) | 0,
+            v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
+
+/* 
+const chat  = {
+  id: {
+    users: [
+      {
+        id: '1',
+        ...other
+      }
+    ],
+    msgs: [
+      {
+        userId: 1,
+        text: ''
+      }
+    ]
+  }
+}
+
+*/
+
+function Room(id) {
+    this.id = id;
+    this.name = '';
+    this.users = [];
+    this.messages = [];
+
+    this.join = function (user) {
+        this.users.push(user);
+    };
+
+    this.out = function (user) {
+        const idx = this.users.findIndex((u) => user.id === u.id);
+        if (idx > -1) {
+            this.users.splice(idx, 1);
+        }
+    };
+}
+
+export default function Store() {
+    this.counter = 1;
+    this.rooms = [];
+    this.users = [];
+
+    this.findOrCreateRoom = function (roomId) {
+        const idx = this.rooms.findIndex((r) => r.id === roomId);
+        const room = this.rooms[idx] || this.createRoom(roomId);
+        if (idx === -1) {
+            this.rooms.push(room);
+        }
+        return room;
+    };
+
+    this.createRoom = function (id) {
+        return new Room(id);
+    };
+
+    this.closeRoom = function (room) {
+        const idx = this.rooms.findIndex((r) => room.id === r.id);
+        if (idx > -1) {
+            this.rooms.splice(idx, 1);
+        }
+    };
+
+    this.createUser = function (name) {
+        const user = {
+            id: uuidv4(),
+            name: name || `user${this.counter++}`
+        };
+
+        this.users.push(user);
+        return user;
+    };
+}
